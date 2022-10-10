350f8709a0f581ab85c4b3659f4e22905b236e78
Sergen <dmrsrgn@gmail.com>
2022-10-10 17:34:02 -0500

    add todos to dashboard component

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,6 +2,14 @@ const Dashboard = () => {
   return (
     <div>
       <h1>dashboard</h1>
+      <form action="">
+        {/* Todo
+            - add inputs.
+            - add post button
+            - hook firestore and upload post
+            - show it on homepage
+        */}
+      </form>
     </div>
   );
 };
