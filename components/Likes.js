8a08d30fa60d57f27117bdce5b06072fb1bcfdd1
sergen demir <dmrsrgn@gmail.com>
2022-10-18 18:27:55 -0500

    user can only vote +1 and -1

--- components/Likes.js
+++ components/Likes.js
@@ -4,7 +4,10 @@ import { AiOutlineArrowUp, AiOutlineArrowDown } from "react-icons/ai";
 import { db } from "../firebase";
 
 const Likes = ({ likes, id }) => {
+  // Disable or toggle like button after voting
   const [currentLikes, setCurrentLikes] = useState(likes);
+  const [upVoted, setUpVoted] = useState(false);
+  const [downVoted, setDownVoted] = useState(false);
 
   const updateLikes = async () => {
     const dumpRef = doc(db, "dumps", id);
@@ -19,7 +22,18 @@ const Likes = ({ likes, id }) => {
   }, [currentLikes]);
   return (
     <div className="flex flex-col items-center justify-center p-2 gap-2 text-teal-50">
-      <button onClick={() => setCurrentLikes((prev) => prev + 1)}>
+      <button
+        onClick={() => {
+          if (upVoted) return;
+          if (downVoted) {
+            setCurrentLikes((prev) => prev + 1);
+            setDownVoted(false);
+            return;
+          }
+          setCurrentLikes((prev) => prev + 1);
+          setUpVoted(true);
+        }}
+      >
         <AiOutlineArrowUp />
       </button>
       <span
@@ -33,7 +47,18 @@ const Likes = ({ likes, id }) => {
       >
         {currentLikes}
       </span>
-      <button onClick={() => setCurrentLikes((prev) => prev - 1)}>
+      <button
+        onClick={() => {
+          if (downVoted) return;
+          if (upVoted) {
+            setCurrentLikes((prev) => prev - 1);
+            setUpVoted(false);
+            return;
+          }
+          setCurrentLikes((prev) => prev - 1);
+          setDownVoted(true);
+        }}
+      >
         <AiOutlineArrowDown />
       </button>
     </div>
