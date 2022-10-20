3fcb5b776cfe29782a5cdacbbfe33fed63c7c1d2
sergen demir <dmrsrgn@gmail.com>
2022-10-20 15:06:32 -0500

    add login with google to signup page

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import { BsGoogle } from "react-icons/bs";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useRouter } from "next/router";
 
 const Signup = () => {
   const [signupForm, setSignupForm] = useState({
     email: "",
     password: "",
   });
+
+  const provider = new GoogleAuthProvider();
+  const route = useRouter();
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -15,6 +22,15 @@ const Signup = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(signupForm);
+  };
+
+  const login = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      route.push("/profile");
+    } catch (error) {
+      console.error(error);
+    }
   };
   return (
     <div className="shadow bg-slate-600 mt-16 p-4 sm:w-full md:w-1/3 mx-auto rounded">
@@ -55,6 +71,15 @@ const Signup = () => {
           Already have an account?
         </button>
       </Link>
+      <div className="py-4 mx-auto text-center">
+        <button
+          className="mx-auto p-2  bg-teal-500 rounded text-teal-50 flex items-center justify-center gap-2 text-center pointer w-full "
+          onClick={login}
+        >
+          <BsGoogle className="text-medium " />
+          Continue with Google
+        </button>
+      </div>
     </div>
   );
 };
