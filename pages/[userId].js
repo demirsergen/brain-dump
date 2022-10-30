521ec72f410f36c4400afe5a80be9d8ac08df159
Sergen Demir <dmrsrgn@gmail.com>
2022-10-30 18:04:53 -0500

    funcitonality to visit other user profile

--- pages/[userId].js
+++ pages/[userId].js
@@ -1,49 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { db, auth } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  query,
+  where,
+  onSnapshot,
+  collection,
+  setLogLevel,
+} from "firebase/firestore";
 import Image from "next/image";
-import Link from "next/link";
 import defaultAvatar from "../public/default-avatar.svg";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Dump from "../components/Dump";
 
 const UserProfile = () => {
-  const [user] = useAuthState(auth);
-  const [userProfile, setUserProfile] = useState();
+  const [user, loading] = useAuthState(auth);
+  const [userPosts, setUserPosts] = useState([]);
   const router = useRouter();
   const { userId } = router.query;
 
-  const getUserInfo = async () => {
-    const userRef = doc(db, "users", userId);
-    const userDoc = await getDoc(userRef);
-    const userInfo = userDoc.data();
+  const getUserPosts = async () => {
+    if (loading) return;
+    if (!user) return router.push("/auth/login");
 
-    setUserProfile(userInfo);
+    const postsRef = collection(db, "dumps");
+    const q = query(postsRef, where("userId", "==", userId));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setUserPosts(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+      return unsubscribe;
+    });
   };
 
   useEffect(() => {
     if (user?.uid === userId) {
       router.push("/profile");
     }
-  });
+  }, []);
 
   useEffect(() => {
-    getUserInfo();
+    getUserPosts();
   }, [userId]);
 
   return (
     <div className="shadow p-2 my-4  bg-slate-600 rounded md:w-1/2 mx-auto">
-      <div className="flex items-center gap-2 border-b-2 pb-2">
-        <Image
-          src={userProfile?.photoURL || defaultAvatar}
-          alt="Picture of the profil owner"
-          width={30}
-          height={30}
-          className="rounded-full"
-        />
-        <h1 className="text-teal-50">
-          {userProfile?.displayName || userProfile?.email}
-        </h1>
+      <h1 className="text-teal-50 text-center">Posts</h1>
+      <div>
+        {userPosts.map((post) => {
+          return <Dump key={post.id} dump={post} />;
+        })}
       </div>
     </div>
   );
