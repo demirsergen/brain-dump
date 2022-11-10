6dcc1aecec93818ff5629ad6eb5039e658d41102
Sergen Demir <dmrsrgn@gmail.com>
2022-11-09 22:26:52 -0600

    add update doc functionality

--- components/userProfile/UserProfileHeader.js
+++ components/userProfile/UserProfileHeader.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import defaultAvatar from "../../public/default-avatar.svg";
 import UserProfileHeaderButtons from "./UserProfileHeaderButtons";
+import { doc, getDoc } from "firebase/firestore";
 
 const UserProfileHeader = () => {
   const [user, loading] = useAuthState(auth);
+  const [currentUser, setCurrentUser] = useState();
+
+  const getUpdatedUserInfo = async () => {
+    const docRef = doc(db, "users", user.uid);
+    const data = await getDoc(docRef);
+    setCurrentUser(data.data());
+  };
+
+  useEffect(() => {
+    getUpdatedUserInfo();
+    console.log(currentUser);
+  }, [user]);
 
   return (
     <div className="flex items-center gap-2 border-b-2 pb-2">
       <Image
-        src={user?.photoURL || defaultAvatar}
+        src={currentUser?.photoURL || defaultAvatar}
         alt="Picture of the profil owner"
         width={30}
         height={30}
         className="rounded-full"
       />
-      <h1 className="text-teal-50">{user?.displayName || user?.email}</h1>
+      <h1 className="text-teal-50">
+        {currentUser?.username || currentUser?.email}
+      </h1>
       <UserProfileHeaderButtons />
     </div>
   );
--- pages/updateprofile.js
+++ pages/updateprofile.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import { doc, updateDoc } from "firebase/firestore";
 
 const UpdateProfile = () => {
   const [user, loading] = useAuthState(auth);
   const [photoUrl, setPhotoUrl] = useState("");
-  const [username, setdisplayName] = useState("");
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+
+  const uploadAvatar = () => {};
+  const updateInfo = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    const docRef = doc(db, "users", user.uid);
+    const data = {
+      username: username,
+    };
+    await updateDoc(docRef, data)
+      .then((docRef) => {
+        setMessage("You've successfully updated your info!");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return (
     <div className="my-4 py-4 bg-slate-600 rounded md:w-1/2 mx-auto">
       <h1 className="text-teal-50 text-center uppercase font-bold">
         Update Info
       </h1>
-      <form className="px-2">
+      <form className="px-4">
         <div className="p-2 rounded">
           <label className="text-teal-50">Username:</label>
           <br />
@@ -21,6 +40,7 @@ const UpdateProfile = () => {
             name="username"
             placeholder="Username"
             className="w-full bg-gray-100 p-2 rounded "
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="p-2 rounded">
@@ -31,6 +51,15 @@ const UpdateProfile = () => {
             className="w-full bg-gray-100 p-2 rounded "
           />
         </div>
+        <button
+          onClick={updateInfo}
+          className="bg-teal-500 p-2 rounded text-teal-50 text-sm block w-full"
+        >
+          Update
+        </button>
+        <span className="text-teal-50 block mx-auto text-center p-4">
+          {message && message}
+        </span>
       </form>
     </div>
   );
