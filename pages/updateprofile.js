e392fca921c4d90dc428db650c15e2f5c5152085
Sergen Demir <dmrsrgn@gmail.com>
2022-11-09 23:08:17 -0600

    add uploading images to storage functionality

--- pages/updateprofile.js
+++ pages/updateprofile.js
@@ -2,14 +2,32 @@ import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
 import { doc, updateDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const UpdateProfile = () => {
   const [user, loading] = useAuthState(auth);
-  const [photoUrl, setPhotoUrl] = useState("");
+  const [file, setFile] = useState();
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
 
-  const uploadAvatar = () => {};
+  const storage = getStorage();
+  console.log(file);
+
+  const uploadAvatar = async (e) => {
+    e.preventDefault();
+    const photosRef = ref(storage, `images/${user.uid}/${file.name}`);
+    try {
+      await uploadBytes(photosRef, file)
+        .then((snapshot) => {
+          setMessage("Upload successful!");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
   const updateInfo = async (e) => {
     e.preventDefault();
     setMessage("");
@@ -43,19 +61,27 @@ const UpdateProfile = () => {
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
+        <button
+          onClick={updateInfo}
+          className="bg-teal-500 p-2 rounded text-teal-50 text-sm block w-full"
+        >
+          Update
+        </button>
+
         <div className="p-2 rounded">
           <label className="text-teal-50">Upload Avatar:</label>
           <input
             type="file"
             name="avatar"
             className="w-full bg-gray-100 p-2 rounded "
+            onChange={(e) => setFile(e.target.files[0])}
           />
         </div>
         <button
-          onClick={updateInfo}
+          onClick={uploadAvatar}
           className="bg-teal-500 p-2 rounded text-teal-50 text-sm block w-full"
         >
-          Update
+          Upload
         </button>
         <span className="text-teal-50 block mx-auto text-center p-4">
           {message && message}
