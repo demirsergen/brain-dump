d680e167a87ab2481b2d6dbc7a71d796c2867c1e
Sergen Demir <dmrsrgn@gmail.com>
2023-01-28 16:42:30 -0600

    use authcontext for news

--- components/news/AllNews.jsx
+++ components/news/AllNews.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import News from './News';
 import { getAllNews } from './utils/getAllNews';
+import { AuthContext } from '../Layout';
 
 const AllNews = () => {
   const [news, setNews] = useState(null);
+
+  const { currentUser } = useContext(AuthContext);
 
   const getData = async () => {
     const data = await getAllNews();
@@ -15,14 +18,16 @@ const AllNews = () => {
     getData();
   }, []);
 
-  return (
-    <div className="bg-slate-600 h-screen text-teal-50 w-1/3 flex flex-col gap-2 p-2 rounded overflow-y-scroll">
-      <h3 className="text-center font-bold">News</h3>
-      {news?.map((news) => (
-        <News key={news.id} news={news} />
-      ))}
-    </div>
-  );
+  if (currentUser) {
+    return (
+      <div className="bg-slate-600 h-screen text-teal-50 w-1/3 flex flex-col gap-2 p-2 rounded overflow-y-scroll">
+        <h3 className="text-center font-bold">News</h3>
+        {news?.map((news) => (
+          <News key={news.id} news={news} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default AllNews;
