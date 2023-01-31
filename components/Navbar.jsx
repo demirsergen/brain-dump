ffbd8b156ed03a28519de1a4c0c04b8199765325
Sergen Demir <dmrsrgn@gmail.com>
2023-01-30 18:08:12 -0600

    change styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
 
   return (
-    <nav className="flex items-center justify-between p-4 md:w-1/2 mx-auto">
+    <nav className="flex items-center justify-between p-4 md:w-1/2 lg:w-2/3 mx-auto">
       <Link href="/">
         <button className=" text-teal-50 font-bold">
           {' '}
