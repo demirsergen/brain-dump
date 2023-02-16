3ff18d838b825fd950fd0187117993672401f233
Sergen Demir <dmrsrgn@gmail.com>
2023-02-15 18:30:26 -0600

    add todo for the component

--- components/AddFriendButton.jsx
+++ components/AddFriendButton.jsx
@@ -1,12 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from './Layout';
 
 const AddFriendButton = () => {
   const { currentUser } = useContext(AuthContext);
+  const [alreadyFriends, setAlreadyFriends] = useState(false);
 
-  const checkFriend = () => {};
+  const checkFriend = () => {
+    // check if friend exist, if it does do not show add button and update state variable
+  };
 
-  return <button>Add</button>;
+  if (alreadyFriends) {
+    return <button>Unfriend</button>;
+  } else {
+    return <button>Add</button>;
+  }
 };
 
 export default AddFriendButton;
