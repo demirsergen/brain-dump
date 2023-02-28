f472e86c41aaea7e9ea33638f7e54db903cf205b
Sergen Demir <dmrsrgn@gmail.com>
2023-02-28 10:55:05 -0600

    configure to get latest news

--- components/news/utils/getAllNews.js
+++ components/news/utils/getAllNews.js
@@ -1,8 +1,14 @@
 export async function getAllNews(query) {
   const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY;
+
+  const date = new Date();
+  let day = date.getDate() - 1;
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
   const url = `https://newsapi.org/v2/everything?q=${
-    query ? query : 'Apple'
-  }&from=2023-01-26&sortBy=popularity&pageSize=10&apiKey=${apiKey}`;
+    query ? query : 'javascript'
+  }&from=${year}-${month}-${day}&sortBy=popularity&pageSize=10&apiKey=${apiKey}`;
 
   const response = await fetch(url);
   const data = await response.json();
