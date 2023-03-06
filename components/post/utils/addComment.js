5ab9e3c4408ae8c7fbb3004fb38fd7367c207648
Sergen Demir <dmrsrgn@gmail.com>
2023-03-05 18:33:07 -0600

    update logic for adding comments

--- components/post/utils/addComment.js
+++ components/post/utils/addComment.js
@@ -4,14 +4,24 @@ import {
   collection,
   addDoc,
   serverTimestamp,
+  setDoc,
 } from 'firebase/firestore';
 
 export const addComment = async (postId, userId, comment) => {
   const postCollectionRef = doc(db, 'posts', postId);
   const commentsRef = collection(postCollectionRef, 'comments');
-  await addDoc(commentsRef, {
+
+  const newComment = {
     comment: comment,
     timestamp: serverTimestamp(),
     userId: userId,
+    postId: postId,
+    commentId: '',
+  };
+  await addDoc(commentsRef, newComment).then((comment) => {
+    const docId = comment.id;
+    newComment.commentId = docId;
+
+    setDoc(comment, newComment);
   });
 };
