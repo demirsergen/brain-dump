ebd8ddd47a856e0504c78f33efc1fc2d0e59c3cc
Sergen Demir <dmrsrgn@gmail.com>
2023-03-31 10:10:56 -0500

    add future directions and ideas

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -6,6 +6,9 @@ import { AuthContext } from '../components/Layout';
 const Profile = () => {
   const { currentUser } = useContext(AuthContext);
 
+  // find a way to incorporate IAC API into this app
+  // make it interesting and related to chicago
+
   if (currentUser) {
     return (
       <div className="shadow p-2 bg-slate-600 rounded md:w-1/2 mx-auto flex-1">
