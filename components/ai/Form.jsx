2e034bae0096bf5c6a4135b63f48498adda53f9f
Sergen Demir <dmrsrgn@gmail.com>
2023-04-15 18:20:24 -0500

    add comment

--- components/ai/Form.jsx
+++ components/ai/Form.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Form = () => {
   // this is where the prompts are going to be entered
+  // create new account to be able use OPENAI
   return <form>Form</form>;
 };
 
