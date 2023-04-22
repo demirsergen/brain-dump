60df54f00927b05be463127cc116d1ef1e248471
Sergen Demir <dmrsrgn@gmail.com>
2023-04-21 20:37:55 -0500

    delete files

--- components/ai/Form.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Form = () => {
-  // this is where the prompts are going to be entered
-  // create new account to be able use OPENAI
-  return <form>Form</form>;
-};
-
-export default Form;
