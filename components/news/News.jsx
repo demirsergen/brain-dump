0aec1192eb8cd6775f05ca72708cb2f3d35a96c7
Sergen Demir <dmrsrgn@gmail.com>
2023-05-04 21:19:40 -0500

    use default image for news to fix bug

--- components/news/News.jsx
+++ components/news/News.jsx
@@ -7,7 +7,7 @@ const News = ({ news }) => {
     return (
       <div className="bg-slate-500 p-2 rounded flex flex-col gap-2 text-sm">
         <Image
-          src={news.urlToImage || defaultNews}
+          src={defaultNews}
           alt="news-image"
           width={250}
           height={100}
