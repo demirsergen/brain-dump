68d9291b86ab2aad220d0dad8ff41147d1cb3833
Sergen Demir <dmrsrgn@gmail.com>
2023-05-27 10:58:15 -0500

    delete comment

--- pages/index.jsx
+++ pages/index.jsx
@@ -52,9 +52,6 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon1.png" />
       </Head>
-
-      {/* change icon!! */}
-
       <div className="bg-slate-600 rounded p-2 flex flex-col gap-2 flex-grow">
         <h1 className="text-teal-50 text-center font-bold">
           Latest Posts
