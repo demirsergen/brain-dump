a911adebb834222d05e86b3337dce8e7428a2c2c
Sergen Demir <dmrsrgn@gmail.com>
2023-06-05 20:14:16 -0500

    create dashboard route

---
+++ pages/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AllPosts from '../components/AllPosts';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <AllPosts />
+    </div>
+  );
+};
+
+export default Dashboard;
