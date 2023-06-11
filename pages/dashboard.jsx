e4682211689630cdfd70471e928426a287761df9
Sergen Demir <dmrsrgn@gmail.com>
2023-06-11 08:49:39 -0500

    add search bar component to dashboard page

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import AllPosts from '../components/AllPosts';
+import SearchBar from '../components/SearchBar';
 
 const Dashboard = () => {
   return (
     <div className="shadow p-2 bg-slate-600 rounded mx-auto w-2/3">
+      <SearchBar />
       <AllPosts />
     </div>
   );
