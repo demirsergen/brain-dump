0b0b07009f56f7353a865bf8009ebdbfea49a2c5
Sergen Demir <dmrsrgn@gmail.com>
2023-06-11 08:49:09 -0500

    create search bar component

---
+++ components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const SearchBar = () => {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = () => {};
+  return (
+    <form onSubmit={handleSubmit} className="flex rounded p-1">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Search for keywords..."
+        className="flex-grow p-1 rounded"
+      />
+      <input
+        type="submit"
+        value="Search"
+        className="p-1 text-white bg-teal-500 rounded"
+      />
+    </form>
+  );
+};
+
+export default SearchBar;
