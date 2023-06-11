bf444ccd8b36923b6973909dd084e31f278889ca
Sergen Demir <dmrsrgn@gmail.com>
2023-06-11 08:25:28 -0500

    update styling

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -4,7 +4,7 @@ const LandingPage = () => {
   return (
     <div className="bg-teal-500 min-h-screen rounded-xl flex items-center justify-center">
       <div className="max-w-md mx-auto p-8 bg-white rounded-lg shadow-lg">
-        <h1 className="text-4xl font-bold text-teal-500 mb-6">
+        <h1 className="text-4xl font-bold text-teal-500 mb-6 text-center">
           Braindump
         </h1>
         <p className="text-lg text-gray-700 mb-8">
@@ -16,12 +16,12 @@ const LandingPage = () => {
           unfiltered thoughts with the world.
         </p>
         <Link href="/auth/login">
-          <button className="bg-orange-500 block my-2 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-orange-500 block my-2 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded mx-auto">
             Get Started
           </button>
         </Link>
         <Link href="/manifest">
-          <button className="bg-orange-500 block hover:bg-orange-600 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-orange-500 block hover:bg-orange-600 text-white font-bold py-2 px-4 rounded mx-auto">
             Read our Manifest
           </button>
         </Link>
