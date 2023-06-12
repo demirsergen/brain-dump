157546825864a85653925b0666a90d6b4b6bf8be
Sergen Demir <dmrsrgn@gmail.com>
2023-06-12 09:06:27 -0500

    update styling

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -5,18 +5,21 @@ const SearchBar = () => {
 
   const handleSubmit = () => {};
   return (
-    <form onSubmit={handleSubmit} className="flex rounded p-1">
+    <form
+      onSubmit={handleSubmit}
+      className="flex rounded p-1 text-sm"
+    >
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="Search for keywords..."
-        className="flex-grow p-1 rounded"
+        className="flex-grow p-1 rounded rounded-r-none"
       />
       <input
         type="submit"
         value="Search"
-        className="p-1 text-white bg-teal-500 rounded cursor-pointer"
+        className="p-1 text-white bg-teal-500 rounded cursor-pointer rounded-l-none"
       />
     </form>
   );
