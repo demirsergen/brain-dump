e1b249317f0fc2a109fbb6d566cedbdf156c0928
Sergen Demir <dmrsrgn@gmail.com>
2023-06-12 09:01:00 -0500

    style footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Footer = () => {
   // add links, make it look prettier
   return (
-    <footer className="flex items-center justify-between mt-4 p-8 md:w-1/2 lg:w-full mx-auto bg-slate-700">
+    <footer className="flex items-center justify-between mt-4 p-4 md:w-1/2 lg:w-full mx-auto bg-slate-700">
       <Link href="/">
         <button className=" text-teal-50 font-bold">
           {' '}
