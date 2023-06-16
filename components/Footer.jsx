c3f1b4a3d4985bbe4c08fd811cbf7337c004da5f
Sergen Demir <dmrsrgn@gmail.com>
2023-06-15 20:12:41 -0500

    delete comment

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 
 const Footer = () => {
-  // add links, make it look prettier
   return (
     <footer className="flex items-center justify-between mt-4 p-4 md:w-1/2 lg:w-full mx-auto bg-slate-700">
       <Link href="/">
