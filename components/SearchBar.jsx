67b19e28148cb25284caa4e0c5c2cd3762c664e8
Sergen Demir <dmrsrgn@gmail.com>
2023-06-24 13:56:08 -0500

    add message if query is empty

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -2,31 +2,40 @@ import React, { useState } from 'react';
 
 const SearchBar = ({ getSearchResults }) => {
   const [query, setQuery] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (query) {
       await getSearchResults(query);
+      setMessage('');
+    } else {
+      setMessage('Enter a tag to search...');
     }
   };
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex rounded p-1 text-sm"
-    >
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search for keywords..."
-        className="flex-grow p-1 rounded rounded-r-none"
-      />
-      <input
-        type="submit"
-        value="Search"
-        className="p-1 text-white bg-teal-500 rounded cursor-pointer rounded-l-none"
-      />
-    </form>
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex rounded p-1 text-sm"
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search for tags..."
+          className="flex-grow p-1 rounded rounded-r-none"
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="p-1 text-white bg-teal-500 rounded cursor-pointer rounded-l-none"
+        />
+      </form>
+      {message && (
+        <p className="text-red-600 px-1 text-sm">{message}</p>
+      )}
+    </>
   );
 };
 
