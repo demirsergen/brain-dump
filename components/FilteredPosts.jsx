a9f07811dcfab22bef9e47bce63b169ef42747de
Sergen Demir <dmrsrgn@gmail.com>
2023-06-24 20:39:43 -0500

    remove props

--- components/FilteredPosts.jsx
+++ components/FilteredPosts.jsx
@@ -2,16 +2,9 @@ import React from 'react';
 import Votes from './Votes';
 import Post from './post/Post';
 
-const FilteredPosts = ({
-  filteredPosts,
-  isFiltered,
-  filterQuery,
-}) => {
+const FilteredPosts = ({ filteredPosts }) => {
   return (
     <div className="shadow p-2 bg-slate-600 rounded mx-auto">
-      {isFiltered && (
-        <p className="text-white text-sm">Filter: {filterQuery}</p>
-      )}
       {filteredPosts.map((post) => (
         <div key={post.id} className={'flex mx-auto w-full'}>
           <Votes post={post} />
