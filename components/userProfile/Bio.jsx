29b6d3bc7bbb29518082d7fc9e5f777edada08a3
Sergen Demir <dmrsrgn@gmail.com>
2023-07-09 13:52:48 -0400

    write bio component

--- components/userProfile/Bio.jsx
+++ components/userProfile/Bio.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const Bio = () => {
+const Bio = ({ user }) => {
   // display user info and add it to profile page
-  return <div>Bio</div>;
+  return (
+    <>
+      <div>
+        <h2>{user.displayname}</h2>
+        <h2>{user.username}</h2>
+        <p>{user.bio}</p>
+      </div>
+    </>
+  );
 };
 
 export default Bio;
