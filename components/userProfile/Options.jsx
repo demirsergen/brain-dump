246c0bf9619112f7a0afde9398fa8fd4af0da8ea
Sergen Demir <dmrsrgn@gmail.com>
2023-07-10 10:17:14 -0400

    create options component

---
+++ components/userProfile/Options.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Options = () => {
+  // TODO
+  // - Three dot icon
+  // - if open => display Edit and Delete buttons
+  return <div>Options</div>;
+};
+
+export default Options;
