2238c8aebbbe338812278ec299b3ab10fbd280f0
Sergen Demir <dmrsrgn@gmail.com>
2023-07-11 16:57:37 -0500

    import options icon

--- components/userProfile/Options.jsx
+++ components/userProfile/Options.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { SlOptions } from 'react-icons/sl';
 
 const Options = () => {
   // TODO
   // - Three dot icon
   // - if open => display Edit and Delete buttons
-  return <div>Options</div>;
+  return (
+    <div>
+      <button className="flex items-center gap-2 text-teal-500 font-medium bg-teal-50 rounded p-1">
+        <SlOptions />
+      </button>
+    </div>
+  );
 };
 
 export default Options;
