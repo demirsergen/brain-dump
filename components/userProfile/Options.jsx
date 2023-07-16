91f49669773dbd4a33a57760081cb0e9ef52b361
Sergen Demir <dmrsrgn@gmail.com>
2023-07-16 11:09:28 -0500

    add isOpen state for options component

--- components/userProfile/Options.jsx
+++ components/userProfile/Options.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import { SlOptions } from 'react-icons/sl';
 
 const Options = () => {
   // TODO
   // - if open => display Edit and Delete buttons
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
-      <button className="flex items-center gap-2 text-teal-500 font-medium bg-teal-50 rounded p-1">
+      <button
+        className="flex items-center gap-2 text-teal-500 font-medium bg-teal-50 rounded p-1"
+        onClick={() => {
+          setIsOpen((prev) => !prev);
+        }}
+      >
         <SlOptions />
       </button>
     </div>
