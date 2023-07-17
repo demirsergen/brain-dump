039f23524033080f7f9ee1a9cb3bd7d92e84f31a
Sergen Demir <dmrsrgn@gmail.com>
2023-07-17 11:00:07 -0500

    handle conditional rendering

--- components/userProfile/Options.jsx
+++ components/userProfile/Options.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react';
 import { CgOptions } from 'react-icons/cg';
+import OptionsOpen from '../OptionsOpen';
 
-const Options = () => {
+const Options = ({ post, setShowModal, setDeleteId }) => {
   // TODO
   // - if open => display Edit and Delete buttons
 
   const [isOpen, setIsOpen] = useState(false);
+
+  if (isOpen)
+    return (
+      <OptionsOpen
+        post={post}
+        setShowModal={setShowModal}
+        setDeleteId={setDeleteId}
+      />
+    );
 
   return (
     <div>
       <button
         className="flex items-center gap-2 text-teal-500 font-medium bg-teal-50 rounded p-1"
         onClick={() => {
+          alert('clicked');
           setIsOpen((prev) => !prev);
         }}
       >
