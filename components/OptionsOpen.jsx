6f573f226e0f4a8f19833f3dd2c3a0fdbc4087bf
Sergen Demir <dmrsrgn@gmail.com>
2023-07-19 23:17:45 -0500

    add style to options

--- components/OptionsOpen.jsx
+++ components/OptionsOpen.jsx
@@ -4,7 +4,7 @@ import { AiFillEdit } from 'react-icons/ai';
 
 const OptionsOpen = ({ post, setDeleteId, setShowModal }) => {
   return (
-    <div className="">
+    <div className="p-2">
       <button
         className="flex items-center gap-2 text-red-500 font-medium bg-teal-50 rounded p-1"
         onClick={() => {
