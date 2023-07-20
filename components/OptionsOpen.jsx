aaa6ca811491a4f1b3c84508a4db3a0366af00ca
Sergen Demir <dmrsrgn@gmail.com>
2023-07-20 11:32:20 -0500

    style options when open

--- components/OptionsOpen.jsx
+++ components/OptionsOpen.jsx
@@ -2,25 +2,38 @@ import Link from 'next/link';
 import { BsFillTrashFill } from 'react-icons/bs';
 import { AiFillEdit } from 'react-icons/ai';
 
-const OptionsOpen = ({ post, setDeleteId, setShowModal }) => {
+const OptionsOpen = ({
+  post,
+  setDeleteId,
+  setShowModal,
+  setIsOpen,
+}) => {
   return (
-    <div className="p-2">
+    <div className=" flex flex-col gap-2 absolute right-1 top-1 rounded bg-teal-500 p-1">
       <button
-        className="flex items-center gap-2 text-red-500 font-medium bg-teal-50 rounded p-1"
-        onClick={() => {
-          setShowModal(true);
-          setDeleteId(post.id);
-        }}
+        className="ml-auto text-white"
+        onClick={() => setIsOpen(false)}
       >
-        <BsFillTrashFill />
-        Delete
+        X
       </button>
-      <Link href={{ pathname: '/addpost', query: post }}>
-        <button className="flex items-center gap-2 text-teal-500 font-medium bg-teal-50 rounded p-1">
-          <AiFillEdit />
-          Edit
+      <div className="flex flex-col gap-2 p-2">
+        <button
+          className="flex items-center gap-2 text-red-500 font-medium bg-teal-50 rounded p-1 text-sm"
+          onClick={() => {
+            setShowModal(true);
+            setDeleteId(post.id);
+          }}
+        >
+          <BsFillTrashFill />
+          Delete
         </button>
-      </Link>
+        <Link href={{ pathname: '/addpost', query: post }}>
+          <button className="flex items-center gap-2 text-teal-500 font-medium bg-teal-50 rounded p-1 text-sm">
+            <AiFillEdit />
+            Edit
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
