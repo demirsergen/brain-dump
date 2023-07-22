44538aa36a8e42a8cb10c79a457345f4d47e4b64
Sergen Demir <dmrsrgn@gmail.com>
2023-07-21 19:51:42 -0500

    update modal box styling

--- components/ModalBox.jsx
+++ components/ModalBox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ModalBox = ({ callback }) => {
   return (
-    <div className="border-2 text-white w-3/4 mx-auto p-4 md:w-1/3 rounded">
+    <div className="border-2 text-white w-3/4 mx-auto p-4 md:w-2/3 rounded">
       <h1 className="text-white bg-red-300 py-2 text-center">
         Are you sure?
       </h1>
